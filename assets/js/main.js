1c49635ff808be297f50dee426a21f26dc336791
TurksoyAz <supercoder@AirvonSperCoder.fritz.box>
2022-08-28 17:28:25 +0200

    lev1_2forms+js_function-your-age

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+// let aktuell = new Date(),
+
+// jahr = aktuell.getFullYear();
+
+// function showAlter() {
+
+// jahresZahl = document.getElementById('jahr').value;
+// alter = jahr - jahresZahl;
+// ausgabe.innerHTML = alter;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+function hesabla(){
+
+    let yaz=document.getElementById("qeyd").value;
+    let proses= 2022 % yaz;
+    adress.innerHTML= proses;
+
+}
+
